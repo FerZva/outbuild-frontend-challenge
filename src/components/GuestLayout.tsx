6f0cc4a5ce25fd2fdc8a933e77c9c086a4f612f7
FerZva <fernandzva2019@outlook.es>
2024-10-12 19:26:28 -0600

    UI Login design done

--- src/components/GuestLayout.tsx
+++ src/components/GuestLayout.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 const GuestLayout = () => {
   return (
-    <div>
-      GuestLayout
-      <Outlet />
+    <div className="guest-layout-page">
+      <div className="guest-layout-page_container">
+        <div className="guest-layout-page_container_header-content">
+          <div className="guest-layout-page_container_header-content_logo-container">
+            <img
+              className="guest-layout-page_container_header-content_logo-container_logo"
+              src={Logo}
+              alt=""
+            />
+          </div>
+          <h1>Welcome back</h1>
+          <p>Please enter your credentials to sign in</p>
+        </div>
+        <div className="guest-layout-page_container_form-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,5 +1,58 @@
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordType = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <form className="guest-layout-page_container_form-content_form-container">
+      <div className="guest-layout-page_container_form-content_form-container_email-container">
+        <p className="guest-layout-page_container_form-content_form-container_email-container_p">
+          <strong>E-Mail Address</strong>
+        </p>
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+          className="guest-layout-page_container_form-content_form-container_email-container_input"
+        />
+        <label
+          htmlFor="email"
+          className="guest-layout-page_container_form-content_form-container_email-container_label"
+        >
+          Please enter a valid email
+        </label>
+      </div>
+      <div>
+        <p className="guest-layout-page_container_form-content_form-container_email-container_p">
+          <strong>Password</strong>
+        </p>
+        <div className="guest-layout-page_container_form-content_form-container_password-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter your password"
+            className="guest-layout-page_container_form-content_form-container_password-container_input"
+          />
+          <div className="guest-layout-page_container_form-content_form-container_password-container_icon">
+            {!showPassword ? (
+              <AiFillEye onClick={handlePasswordType} />
+            ) : (
+              <AiFillEyeInvisible onClick={handlePasswordType} />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="guest-layout-page_container_form-content_form-container_button-container">
+        <button className="guest-layout-page_container_form-content_form-container_button-container_button">
+          <strong>Sign In</strong>
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
